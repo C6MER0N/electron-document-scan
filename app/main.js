414035791acfc741139fc007bfed9e41c2d6ed38
Cameron James <cameronryanjames@gmail.com>
2022-06-06 23:24:45 -0400

    Resource files and UI improvments
    added resource files and UI improvements with bootstrap.

--- app/main.js
+++ app/main.js
@@ -9,7 +9,7 @@ let mainWindow;
 function createWindow() {
     // Create the browser window.
     mainWindow = new BrowserWindow({ width: 1024, height: 1024, resizable: true });
-
+    
     // and load the index.html of the app.
     mainWindow.loadURL('file://' + __dirname + '/index.html');
 
